fd24205b67970f2d80a8716198aca3f572288b22
Loshani Ramsha <loshani.r@boswingroup.com>
2025-12-10 11:36:42 +0530

    feat: implement main application flow using BookFactory and LibraryService

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import org.example.factory.BookFactory;
+import org.example.model.Book;
+import org.example.service.LibraryService;
+import org.example.service.impl.LibraryServiceImpl;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to the Book Library Application!");
+
+        LibraryService library = new LibraryServiceImpl();
+
+        // Using Factory Pattern to create books
+        Book b1 = BookFactory.createBook("fiction", "The Hobbit", "J.R.R. Tolkien", 15.99);
+        Book b2 = BookFactory.createBook("non-fiction", "Sapiens", "Yuval Noah Harari", 20.50);
+
+        library.addBook(b1);
+        library.addBook(b2);
+
+        System.out.println("Book added!" + "Book 1 is : " + b1);
+        System.out.println("Book added!" + "Book 2 is : " + b2);
+
     }
 }
